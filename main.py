c1468ccdfae3189e533f3b44f5f29a5337a0f2db
inlfwetrust <inlfwetrust@gmail.com>
2022-05-15 21:00:41 +1000

    change

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 class User:
+    id = 1
+
     def __init__(self, name, age, sex, strength=10):
         self.name = name
         self.age = age
         self.sex = sex
         self.strength = strength
-        self.id = 0
+        self.id = User.id
+        User.id += 1
 
 
 user_bob = User('Bob', 32, 'male')
 user_maria = User('Mari', 66, 'Female', 25)
+user_jack = User('Jack', 23, 'Female', 1)
 
-print(user_bob.strength)
-print(user_maria .id)
+
+print(user_bob.id)
+print(user_maria.id)
+print(user_jack.id)
 
