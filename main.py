006d075395df615b12224eb2e062a6fe52811e4f
inlfwetrust <inlfwetrust@gmail.com>
2022-05-15 20:40:08 +1000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+class User:
+    def __init__(self, name, age, sex, strength=10):
+        self.name = name
+        self.age = age
+        self.sex = sex
+        self.strength = strength
+
+
+user_bob = User('Bob', 32, 'male')
+user_maria = User('Mari', 66, 'Female', 25)
+
+print(user_bob.strength)
+print(user_maria .strength)
+
